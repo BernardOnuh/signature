70e317d8d24c67fe678f47ba418a8b16e593f716
BernardOnuh <onuhbernard4@gmail.com>
2023-03-19 22:12:26 -0700

    first

---
+++ constants/index.tsx
@@ -0,0 +1,20 @@
+export const navLinks = [
+    {
+      id: "home",
+      title: "Home",
+    },
+    {
+      id: "features",
+      title: "Features",
+    },
+    {
+      id: "product",
+      title: "Product",
+    },
+    {
+      id: "clients",
+      title: "Clients",
+    },
+  ];
+  
+  

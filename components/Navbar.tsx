36cf9e5b7597ead2862b2dc9190786cf677f5404
BernardOnuh <onuhbernard4@gmail.com>
2023-03-19 23:20:03 -0700

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -37,6 +37,19 @@ const Navbar = () => {
                     !toggle ? 'hidden':'flex'
                     } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-x1 sidebar`}
                     >
+                        <ul className='list-none flex justify-end items-start flex-1 flex-col'>
+                            {navLinks.map((nav,index) => (
+                                <li
+                                key={nav.id}
+                                className={`cursor-pointer text-[16px] ${
+                                    active === nav.title ? 'text-white' : 'text-dimWhite'}
+                                    ${index === navLinks.length -1 ? 'mb-0':'mb-4'}`}
+                                    onClick={() => setActive(nav.title)}
+                                >
+                                    <a href={`#${nav.id}`}>{nav.title}</a>
+                                </li>
+                            ))}
+                        </ul>
 
                 </div>
             </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Navbar from '../components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +16,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        <Navbar />
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;

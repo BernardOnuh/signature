212ca7af51f5d1a5629bb8f4dd8d18bcf26fab04
BernardOnuh <onuhbernard4@gmail.com>
2023-03-21 13:53:07 -0700

    carosel

---
+++ components/idolo.tsx
@@ -0,0 +1,4 @@
+import Image from 'next/image';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import {  ademi, bms, bson, coach, cryptoL, DefiQ, dehkunle, dfantom, feyi, noble, ohjay, prudent, quiv, sarah, xeus } from '@/public/idolos';
---
+++ public/idolos/index.tsx
@@ -0,0 +1,33 @@
+import ademi from './ademi.png'
+import bms from './bms.png'
+import bson from './bson.png'
+import coach from './c-coach.png'
+import cryptoL from './cryptoL.png'
+import DefiQ from './DefiQ.png'
+import dehkunle from './dehkunle.png'
+import dfantom from './dfantom.png'
+import feyi from './feyi.png'
+import noble from './noble.png'
+import ohjay from './ohjay.png'
+import prudent from './prudent.png'
+import quiv from './quiv.png'
+import sarah from './sarah.png'
+import xeus from './xeus.png'
+
+export {
+    ademi,
+    bms,
+    bson,
+    coach,
+    cryptoL,
+    DefiQ,
+    dehkunle,
+    dfantom,
+    feyi,
+    noble,
+    ohjay,
+    prudent,
+    quiv,
+    sarah,
+    xeus
+}

971811b6967b31312f1c697d8787dca34fc6a4b5
BernardOnuh <onuhbernard4@gmail.com>
2023-03-24 15:48:30 -0700

    contract

--- components/MyNFT.tsx
+++ components/MyNFT.tsx
@@ -2,7 +2,7 @@ import { features } from '../constants'
 import styles,{ layout } from '../styles/style'
 import Button from "./Button";
 
-const FeatureCard = ({icon, title, content, index}) => (
+const FeatureCard = ({ icon, title, content, index }:any) => (
     <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
     <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
       <img src={icon} alt="star" className="w-[50%] h-[50%] object-contain" />
